7be2bfaaf1eca4b5f6f721f065dec1cbf1b74bd6
davidling0114 <0114lindavid@gmail.com>
2017-06-26 20:36:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+let answer = document.getElementById('answer');
+let attempt = document.getElementById('attempt');
+
+function guess() {
+    let input = document.getElementById('user-guess');
+    if(answer.value == '' || attempt.value == ''){
+      setHiddenFields();
+    }
+    if(!validateInput(input.value)){
+      return 0;
+    }
+    attempt.value++;
+    if(getResults(input.value)){
+      setMessage('You Win! :)');
+      showAnswer(true);
+      showReplay();
+    }
+    else if(attempt.value >= 10){
+      setMessage('You Lose! :(');
+      showAnswer(false);
+      showReplay();
+    }
+    else{
+      setMessage('Incorrect, try again.');
+    }
+}
+
+function getResults(input){
+let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+for(var i = 0; i < input.length; i++){
+  if(input.value.charAt(i) == answer.value.charAt(i)){
+    html += '<span class="glyphicon glyphicon-ok"></span>';
+  }
+  else if(answer.value.indexof(input.charAt(i)) > -1){
+    html += '<span class="glyphicon glyphicon-transfer"></span>';
+  }
+  else{
+    html += '<span class="glyphicon glyphicon-remove"></span>';
+  }
+}
+  html += '<div></div>';
+  document.getElementById('results').innerHTML += html;
+
+  if(input == answer.value){
+    return true;
+  }
+  return false;
+}
+//implement new functions here
+function setHiddenFields(){
+  answer.value = Math.floor(Math.random() * 10000).toString();
+  while(answer.value.length < 4){
+    answer.value = "0" + answer.value;
+  }
+  attempt.value = "0";
+}
+function showAnswer(success){
+  let code = document.getElementById('code');
+  if(success){
+    code.className += ' success';
+  }
+  else{
+    code.className += ' failure';
+  }
+  code.innerHTML = answer.value;
+}
+function showReplay(){
+document.getElementById('guessing-div').style.display = "none";
+document.getElementsById('replay-div').style.display = "block";
+
+}
+
+function setMessage(message){
+  document.getElementsById('message').innerHTML = message;
+}
+
+function validateInput(input){
+  if(input.length != 4){
+    setMessage('Guesses must be exactly 4 characters long.');
+    return false;
+  }
+  reutrn true;
+}
